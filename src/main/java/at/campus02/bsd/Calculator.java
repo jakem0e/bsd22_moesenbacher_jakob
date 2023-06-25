806b638fcb66f8ca4a10ce157d8126ec8ad6d81f
Jakob Moesenbacher <s50749.edu.campus02.at>
2023-06-25 21:22:37 +0200

    Adding Documentation \n Adding Descreption for the methods to provide information for other developers The pom.xml was also updated to integrate javadoc

--- src/main/java/at/campus02/bsd/Calculator.java
+++ src/main/java/at/campus02/bsd/Calculator.java
@@ -1,23 +1,54 @@
 package at.campus02.bsd;
-
+/**
+ * @author Jakob Mösenbacher
+ */
 public class Calculator {
 
+    /**
+    * <p> This Method creates the sum of two numbers </p>
+    * @param number1 getting fist number for the operation
+     * @param number2 getting second number for the operation
+     * @return the result of the operation
+    */
     public static double add(double number1, double number2){
         return number1 + number2;
     }
+    /**
+     * <p> This Method creates the difference of these numbers </p>
+     * @param number1 getting fist number for the operation
+     * @param number2 getting second number for the operation
+     * @return the result of the operation
+     */
     public static double minus(double number1, double number2){
         return number1 - number2;
     }
+    /**
+     * <p> This Method creates the quotient of two numbers </p>
+     * @param number1 getting fist number for the operation
+     * @param number2 getting second number for the operation
+     * @throws IllegalArgumentException if the second parameter is 0 then the exception is thrown
+     * @return the result of the operation
+     */
     public static double divide(double number1, double number2){
         if(number2 == 0){
             throw new IllegalArgumentException("Argument 'divisor' is 0");
         }
         return number1/number2;
     }
+    /**
+     * <p> This Method creates the product of two numbers </p>
+     * @param number1 getting fist number for the operation
+     * @param number2 getting second number for the operation
+     * @return the result of the operation
+     */
     public static double multiply(double number1, double number2){
         return number1 * number2;
     }
-
+    /**
+     * <p> This Method creates the factorial of a number</p>
+     * @param number1 getting fist number for the operation
+     * @return the result of the operation
+     */
      public static double factorial(double number1){
         if(number1 < 0){
             return 0;
